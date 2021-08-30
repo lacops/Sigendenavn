0b8d3437951aa9a6b4bbda27d9cec25a923482f4
Christian <christian@lacops.dk>
2021-08-30 09:39:31 +0200

    Add project files.

---
+++ HelloWorld.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sigendenavn
+{
+    class HelloWorld
+    {
+        // Property to show on console
+        private string textToShow;
+
+        public string TextToShow
+        {
+            get
+            {
+                return textToShow;
+            }
+            set
+            {
+                textToShow = value;
+            }
+        }
+
+        public void Print()
+        {
+            Console.WriteLine(textToShow);
+            
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Sigendenavn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+
+        HelloWorld hello = new HelloWorld();
+            hello.TextToShow = "Værdi";
+            hello.Print();
+
+            //Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
